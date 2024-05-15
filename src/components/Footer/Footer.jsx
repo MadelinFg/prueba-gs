a7c81b23ef3196fe75e5de116a70de84a602b98d
Madelin FG <84932921+MadelinFg@users.noreply.github.com>
2024-05-14 21:48:28 -0500

    Add Header and Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer({}) {
+    return (
+        <div className="footer">
+            <p className='footer-text'>Grupo Salinas</p>
+            <div className="yellow-divider"></div>
+            <p className='footer-text'>Actualización Octubre 2021</p>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import './Header.css';
+import hamburguesa from '../../assets/svg/hamburguesa.svg';
+import home from '../../assets/svg/home.svg';
 
 function Header({}) {
     return (
         <div className="header">
-            header
+            <div className="left-section">
+                <div className="menu">
+                    <img src={hamburguesa} alt="Menu" className='menu-icon' />
+                </div>
+                <div className="home">
+                    <img src={home} alt="Inicio" className='home-icon' />
+                </div>
+            </div>
+            <div className="right-section">
+                <div className="account">
+                    <img src="https://picsum.photos/200" alt="Cuenta" className='account-icon' />
+                </div>
+            </div>
         </div>
     );
 }
